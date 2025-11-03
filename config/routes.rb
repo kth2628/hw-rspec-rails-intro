d86667cc858f9649a3700bf762601db2f2d997ad
Kim Hoang <kth2628@berkeley.edu>
2025-11-03 02:04:16 +0000

    part 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rottenpotatoes::Application.routes.draw do
   resources :movies
+  get '/search', to: 'movies#search_tmdb', as: 'search_tmdb'
   # map '/' to be a redirect to '/movies'
   root to: redirect('/movies')
 end
